81b20ae84e93a575064e78d905c697b832b79330
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-08 23:57:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  when string[0] == "s"
-    do 10 * "s" + string
+  if string[0] == "s"
+    10 * "s" + string
   else
     string
   end
