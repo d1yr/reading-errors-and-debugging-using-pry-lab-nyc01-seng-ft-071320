889c424a65b6c6d91e7a0f6db83af168a35fcbbf
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-08 23:41:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,8 +12,11 @@ end
 def selection(num)
   if num = 1
     "YUM YUM MUNCH MUNCH MUNCH"
+    require 'pry'
   elsif num  2
     "HAM HAM HAM IN MY TUMMY"
+    binding.pry
+    puts 'hello'
   end
 end
 
