d2bae4258599ba5e62525ea54e1b3329a5397e2c
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-09 00:05:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,12 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
-    puts 'hello'
+   count = 0
+    while count < 10 do
+      string.prepend("s")
+      count += 1
+    end
+    string
   else
    string
   end
